93d92b17157091dbf01af116172a6f29d39eb6cb
Dylan Schmittle <39482716+dylanschmittle@users.noreply.github.com>
2020-10-14 10:32:17 -0400

    Update alias.zsh

--- alias.zsh
+++ alias.zsh
@@ -1,5 +1,5 @@
 # Print All Net Routing and Socket Info
-alias net-info="sudo netstat -tulp && sudo lsof -i -n | egrep 'COMMAND|LISTEN|UDP' && sudo route -ee -v -4 -6 && sudo netstat -rn -6 -4 && sudo ip addr"
+alias net-info="sudo netstat -tulp 2>/dev/null && sudo lsof -i -n 2>/dev/null | egrep 'COMMAND|LISTEN|UDP' && sudo route -ee -v -4 -6 2>/dev/null && sudo netstat -rn -6 -4 2>/dev/null && sudo ip addr 2>/dev/null"
 
 # Look For Suid Bins and Print the Linked Modules
-alias get-suid="find / -type f -perm -u=s 2>/dev/null | xargs ls -1 | xargs ldd"
+alias get-suid="find / -type f -perm -u=s 2>/dev/null | xargs ls -1 | xargs ldd 2>/dev/null"
